b95d4b213094998ea479e2c5d932826af62e06df
Yabets Muhaba <104789624+JABEZ8650@users.noreply.github.com>
2023-12-04 15:48:08 +0300

    Added solution for Print Words Vertically

---
+++ week1/python_track/print-words-vertically.py
@@ -0,0 +1,26 @@
+class Solution:
+    def printVertically(self, s: str) -> List[str]:
+        yo = s.split()
+        max = 0
+        for i in yo:
+            if len(i)>max: max = len(i)
+        m = {}
+        for i in yo:
+            for j in range(max):
+                try:
+                    if j not in m:
+                        m[j] = i[j]
+                    else:
+                        m[j]+=i[j]
+                except:
+                    if j not in m:
+                        m[j] = " "
+                    else:
+                        m[j]+=" "
+                    
+
+        res = []
+        for i in m:
+            res.append(m[i].rstrip())
+        return res
+       
