8c5edc37356cf2cd5882acaa640667173648d303
Yabets Muhaba <104789624+JABEZ8650@users.noreply.github.com>
2024-01-11 07:22:15 +0300

    Added solution for Long Pressed Name

--- week1/python_track/long-pressed-name.py
+++ week1/python_track/long-pressed-name.py
@@ -2,11 +2,6 @@ from itertools import groupby, zip_longest
 
 
 class Solution:
-	"""
-	Time:   O(max(n, m))
-	Memory: O(1)
-	"""
-
 	def isLongPressedName(self, name: str, typed: str) -> bool:
 		for (a, a_gr), (b, b_gr) in zip_longest(groupby(name), groupby(typed), fillvalue=(None, None)):
 			if a != b or sum(1 for _ in a_gr) > sum(1 for _ in b_gr):
