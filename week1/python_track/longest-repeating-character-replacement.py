7501d3027d331a5b41517b2b20a1373379cf3b20
Yabets Muhaba <104789624+JABEZ8650@users.noreply.github.com>
2024-01-13 09:08:56 +0300

    Added solution for Longest Repeating Character Replacement

---
+++ week1/python_track/longest-repeating-character-replacement.py
@@ -0,0 +1,12 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        hm={}
+        ans=0
+        l=0
+        for r in range(len(s)):
+            hm[s[r]]=1+hm.get(s[r],0)
+            while (r-l+1)-max(hm.values())>k:
+                hm[s[l]]-=1
+                l+=1
+            ans=max(ans,r-l+1)
+        return ans
