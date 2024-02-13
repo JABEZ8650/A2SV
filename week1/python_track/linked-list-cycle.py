c3da3a15505e081c9f6ac25e054d125a0d4dcf3a
Yabets Muhaba <104789624+JABEZ8650@users.noreply.github.com>
2024-02-13 15:24:17 +0300

    Added solution for Linked List Cycle

--- week1/python_track/linked-list-cycle.py
+++ week1/python_track/linked-list-cycle.py
@@ -6,15 +6,13 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-    
-        fast = head
-        slow = head
-        
+        slow = fast = head
+
         while fast and fast.next:
-            fast = fast.next.next
-            slow = slow.next
-            
-            if fast == slow:
+            fast= fast.next.next
+            slow=slow.next
+
+            if fast== slow:
                 return True
-    
+        
         return False
