2e68c0156cd450e4d09b311cbedde2301e68bd29
Yabets Muhaba <104789624+JABEZ8650@users.noreply.github.com>
2024-02-13 18:40:09 +0300

    Added solution for Design HashSet

---
+++ week1/python_track/design-hashset.py
@@ -0,0 +1,21 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.dict={}
+
+
+    def add(self, key: int) -> None:
+        self.dict[key]=1
+
+    def remove(self, key: int) -> None:
+        self.dict[key]=0
+
+    def contains(self, key: int) -> bool:
+        return self.dict.get(key,0)!=0
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
