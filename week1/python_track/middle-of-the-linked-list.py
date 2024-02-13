5ff9f6f9059b9e1804bf6555afb0aca2381971b6
Yabets Muhaba <104789624+JABEZ8650@users.noreply.github.com>
2024-02-13 15:23:20 +0300

    Added solution for Middle of the Linked List

--- week1/python_track/middle-of-the-linked-list.py
+++ week1/python_track/middle-of-the-linked-list.py
@@ -5,12 +5,10 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-
         slow = fast = head
 
         while fast and fast.next:
-
             fast = fast.next.next
             slow = slow.next
-
+        
         return slow
