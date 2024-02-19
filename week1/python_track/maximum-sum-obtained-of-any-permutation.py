ebd722270e863d2d8303ac970673e35302e1c57a
Yabets Muhaba <104789624+JABEZ8650@users.noreply.github.com>
2024-02-19 11:02:50 +0300

    Added solution for Maximum Sum Obtained of Any Permutation

--- week1/python_track/maximum-sum-obtained-of-any-permutation.py
+++ week1/python_track/maximum-sum-obtained-of-any-permutation.py
@@ -1,30 +1,17 @@
 class Solution:
-    def maxSumRangeQuery(self, nums: List[int], req: List[List[int]]) -> int:
-        l=[0]*(len(nums)+1)
+    def maxSumRangeQuery(self, nums: List[int], requests: List[List[int]]) -> int:
+        len_nums = len(nums)
+
+        frequencies = [0] * (len_nums + 1)
+        for start, end in requests:
+            frequencies[start] += 1
+            frequencies[end + 1] -= 1
+        for k in range(1, len_nums):
+            frequencies[k] += frequencies[k - 1]
+
+        frequencies.sort(reverse=True)
         nums.sort(reverse=True)
-        for i in req:
-            l[i[0]]+=1
-            l[i[1]+1]-=1
-        for i in range(1,len(l)):
-            l[i]+=l[i-1]
-        l=l[:-1]    
-        d=collections.defaultdict(list)
-        for i in range(len(l)):
-            d[l[i]].append(i)
-        di=collections.OrderedDict(sorted(d.items()))   
-        k=0
-        ans=[0]*len(nums)
-        for i in di:
-            for j in di[i]:
-                ans[j]=nums[len(nums)-k-1]
-                k+=1        
-        c=0
-        for i in range(1,len(ans)):
-            ans[i]+=ans[i-1]
-        
-        ans.insert(0,0)   
-        for i in req:
-            c+=(ans[i[1]+1]-ans[i[0]])
-        return c%(10**9+7)
-        
-            
+        max_sum = 0
+        for num, frequency in zip(nums, frequencies):
+            max_sum += num * frequency
+        return max_sum % (10**9 + 7)
