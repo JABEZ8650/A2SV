1bc2f9a43367ab4bfadddb91985df4ed738a59a0
Yabets Muhaba <104789624+JABEZ8650@users.noreply.github.com>
2024-02-28 15:24:47 +0300

    Add solution for Subsets

---
+++ leetcode/subsets.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(i, path, length):
+            if len(path) == length:
+                ans.append(path[:])
+                return
+            
+            for j in range(i, N):
+                path.append(nums[j])
+                backtrack(j + 1, path, length)
+                path.pop()
+        
+        ans = []
+        N = len(nums)
+        
+        for l in range(N + 1):
+            backtrack(0, [], l)
+        return ans
